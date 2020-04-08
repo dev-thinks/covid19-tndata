2aa053734887e24305ea812ca1dd8459f2506acf
dev-thinks <dev-thinks@gituser.com>
2020-04-07 23:54:43 -0400

    legend and title

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -10,6 +10,7 @@ import { PopUpService } from '../_services/pop-up.service';
 })
 export class MapComponent implements AfterViewInit {
   private map;
+  private info;
   private states;
 
   constructor(private shapeService: ShapeService, private popupService: PopUpService) { }
@@ -45,6 +46,105 @@ export class MapComponent implements AfterViewInit {
     this.map.scrollWheelZoom.disable();
     this.map.boxZoom.disable();
     this.map.keyboard.disable();
+
+    this.info = L.control();
+
+    this.info.onAdd = function (map) {
+      this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+      this.update();
+      return this._div;
+    };
+
+    // method that we will use to update the control based on feature properties passed
+    this.info.update = function (props) {
+      this._div.innerHTML = '<h4>Tamil Nadu Status</h4>' + (props ?
+        '<b>' + props.district + '</b><br />Total Cases:' + props.totalCases + ' ~~ New Cases: ' + props.newCases
+        : 'Hover over a district');
+    };
+
+    this.info.addTo(this.map);
+
+    L.Legend = L.Control.extend({
+      'onAdd': function (map) {
+
+        // add reference to mapinstance
+        map.legend = this;
+
+        const v1 = 10;
+        const v2 = 11;
+        const v3 = 100;
+
+        // create container
+        var container = L.DomUtil.create('div', 'legend');
+
+        const labels = [
+          '<= ' + v1 + ' cases',
+          'Between' + v1 + ' and ' + v2 + ' cases',
+          '> ' + v3 + ' cases'
+        ];
+        const grades = [v1 + 1, v2 + 1, v3];
+        container.innerHTML = '<div><b>Legend</b></div>';
+        for (let i = 0; i < grades.length; i++) {
+          container.innerHTML += '<i style="background:blue"> &nbsp; &nbsp;</i> &nbsp; &nbsp;'
+            + labels[i] + '<br/>';
+        }
+
+        // if content provided
+        if (this.options.content) {
+          // set content
+          container.innerHTML = this.options.content;
+
+        }
+        return container;
+      },
+      'onRemove': function (map) {
+
+        // remove reference from mapinstance
+        delete map.legend;
+
+      },
+
+      // new method for setting innerHTML
+      'setContent': function (str) {
+        this.getContainer().innerHTML = str;
+      }
+    });
+
+    this.map.addControl(new L.Legend({
+      'position': 'topright',
+      'content': this.getLegendContent()
+    }));
+
+  }
+
+  private getLegendContent(){
+    const v1 = 10;
+    const v2 = 11;
+    const v3 = 100;
+
+    // create container
+    var container = document.createElement('div');
+    container.className = 'legend';
+
+    const labels = [
+      '<= ' + v1 + ' cases',
+      'Between' + v1 + ' and ' + v2 + ' cases',
+      '> ' + v3 + ' cases'
+    ];
+    const grades = [v1 + 1, v2 + 1, v3];
+    container.innerHTML = '';
+    for (let i = 0; i < grades.length; i++) {
+      container.innerHTML += '<i style="background:' + this.getColor(grades[i], v3, v1) + '"> &nbsp; &nbsp;</i> &nbsp; &nbsp;'
+        + labels[i] + '<br/>';
+    }
+
+    return container.innerHTML;
+  }
+
+  private getColor(value, max, min) {
+    const val = (value - min) / (max - min);
+    const hue = (val * 120).toString(10);
+    return ['hsl(', hue, ',100%,50%)'].join('');
   }
 
   private initStatesLayer() {
@@ -80,6 +180,8 @@ export class MapComponent implements AfterViewInit {
     var content = this.popupService.makeCapitalPopup(feature);
 
     layer.bindPopup(content);
+
+    this.info.update(feature.properties);
   }
 
   private resetFeature(e) {
@@ -91,6 +193,8 @@ export class MapComponent implements AfterViewInit {
       fillOpacity: 0.8,
       fillColor: '#6DB65B'
     });
+
+    this.info.update();
   }
 
 }
